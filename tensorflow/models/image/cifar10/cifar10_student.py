64481f5fed683d8d701ff7ba97e40f0a1581f667
Joey McDonald <josephpmcdonald@gmail.com>
2016-06-02 15:09:43 -0500

    add student model to train simultaneously

--- tensorflow/models/image/cifar10/cifar10_student.py
+++ tensorflow/models/image/cifar10/cifar10_student.py
@@ -140,9 +140,6 @@ def train():
     # Create a saver.
     saver = tf.train.Saver(tf.all_variables())
 
-    # Build the summary operation based on the TF collection of Summaries.
-    summary_op = tf.merge_all_summaries()
-
     # Build an initialization operation to run below.
     init = tf.initialize_all_variables()
 
@@ -153,9 +150,6 @@ def train():
 
     # Start the queue runners.
     #tf.train.start_queue_runners(sess=sess)
-
-    summary_writer = tf.train.SummaryWriter(FLAGS.train_dir,
-                                            graph_def=sess.graph_def)
 
     images_path = os.path.join(FLAGS.data_dir, 'img.npz')
     logits_path = os.path.join(FLAGS.train_dir, 'log.npz')
@@ -195,9 +189,101 @@ def train():
         print (format_str % (datetime.now(), step, st_loss_value,
                              examples_per_sec, sec_per_batch))
 
-      if step % 100 == 0:
-        summary_str = sess.run(summary_op, feed_dict=feed_dict)
-        summary_writer.add_summary(summary_str, step)
+      # Save the model checkpoint periodically.
+      if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+        checkpoint_path = os.path.join(FLAGS.train_dir, 'model_student.ckpt')
+        saver.save(sess, checkpoint_path, global_step=step,
+                   latest_filename='checkpoint_student')
+
+
+def train_simul():
+  """Train CIFAR-10 for a number of steps."""
+  with tf.Graph().as_default():
+    with tf.variable_scope('model') as m_scope:
+      global_step = tf.Variable(0, trainable=False)
+    with tf.variable_scope('student') as s_scope:
+      st_global_step = tf.Variable(0, trainable=False)
+
+    # Get images and labels for CIFAR-10.
+    with tf.device('/cpu:0'):
+      images, labels = cifar10.distorted_inputs()
+
+    with tf.variable_scope(m_scope):
+      # Build a Graph that computes the logits predictions from the
+      # inference model.
+      logits = cifar10.inference(images)
+      targets = cifar10.multinomial(logits) 
+
+      # Calculate loss.
+      loss = cifar10.loss(logits, labels)
+
+      # Build a graph that trains the model with one batch of examples
+      # and updates the model parameters.
+      train_op = cifar10.train(loss, global_step)
+
+    with tf.variable_scope(s_scope):
+      # Student graph that computes the logits predictions from the
+      # inference model.
+      st_logits = cifar10.inference(images)
+
+      # Calculate loss according to multinomial sampled labels
+      st_loss = cifar10.loss(st_logits, targets)
+
+      # Build a Graph that trains the model with one batch of examples
+      # and updates the model parameters.
+      st_train_op = cifar10.train(st_loss, st_global_step)
+
+    # Create a saver.
+    saver = tf.train.Saver(tf.all_variables())
+
+    # Build an initialization operation to run below.
+    init = tf.initialize_all_variables()
+
+    # Start running operations on the Graph.
+    sess = tf.Session(config=tf.ConfigProto(
+        log_device_placement=FLAGS.log_device_placement))
+    sess.run(init)
+
+    # Start the queue runners.
+    #tf.train.start_queue_runners(sess=sess)
+
+    images_path = os.path.join(FLAGS.data_dir, 'img.npz')
+    logits_path = os.path.join(FLAGS.train_dir, 'log.npz')
+
+    if not tf.gfile.Exists(images_path):
+      raise ValueError('Failed to find file: ' + images_path)
+    if not tf.gfile.Exists(logits_path):
+      raise ValueError('Failed to find file: ' + logits_path)
+
+    with np.load(images_path) as data:
+      images_set = data['images_set']
+      print ('images_set shape type ', images_set.shape, images_set.dtype)
+    with np.load(logits_path) as data:
+      logits_set = data['logits_set']
+      # Normalize logits by mean
+      logits_set = logits_set - np.mean(logits_set, axis=1, keepdims=True)
+      print ('logits_set shape type ', logits_set.shape, logits_set.dtype)
+
+    data_set = Dataset(images_set, logits_set)
+
+    for step in xrange(FLAGS.max_steps):
+      #print (step)
+      start_time = time.time()
+      feed_dict = fill_feed_dict(data_set, images, targets)
+      _, st_loss_value = sess.run([st_train_op, st_loss], feed_dict=feed_dict)
+      duration = time.time() - start_time
+
+      assert not np.isnan(st_loss_value), 'Model diverged with loss = NaN'
+
+      if step % 10 == 0:
+        num_examples_per_step = FLAGS.batch_size
+        examples_per_sec = num_examples_per_step / duration
+        sec_per_batch = float(duration)
+
+        format_str = ('%s: step %d, st_loss = %.2f (%.1f examples/sec; %.3f '
+                      'sec/batch)')
+        print (format_str % (datetime.now(), step, st_loss_value,
+                             examples_per_sec, sec_per_batch))
 
       # Save the model checkpoint periodically.
       if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
